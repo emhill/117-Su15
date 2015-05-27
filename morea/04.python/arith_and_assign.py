715538a7d4801fc53dbf8c10d12bd8898a5d17c6
emily <emily.m.hill@gmail.com>
2015-05-27 12:07:50 -0400

    hw 3

---
+++ morea/04.python/arith_and_assign.py
@@ -0,0 +1,14 @@
+# Demonstrates arithmetic operations and 
+# assignment statements
+
+x = 3
+y = 5
+
+print("x =", x)
+print("y =", y)
+
+print("x * y =", x*y)
+
+# alternatively:
+# result = x * y
+# print("x*y =", result)
