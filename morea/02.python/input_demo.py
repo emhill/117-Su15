3f1201846353e14fa5fa01225065f51e9926b252
emily <emily.m.hill@gmail.com>
2015-07-05 14:45:15 -0400

    unix split

--- morea/02.python/input_demo.py
+++
@@ -1,8 +0,0 @@
-# Demonstrate numeric and string input
-
-color = input("What is your favorite color? ")
-print("Cool!  My favorite color is _light_", color, "!")
-
-rating = eval(input("On a scale of 1 to 10, how much do you like Urban Fire? "))
-print("Cool!  I like it", rating*1.8, "!")
-
